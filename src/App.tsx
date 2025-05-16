bc472a382339c640b6cc81226f1c70172ae966bf
hosomatu <kcfdmori0759@gmail.com>
2025-05-16 15:16:59 +0900

    ログイン機能を削除

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,6 @@ const historyItems = [
 
 // ルートコンポーネントの定義。main.tsxで<App />として呼び出される
 function App() {
-  const { user, signOut } = useAuthenticator();
 
   return (
     <>
@@ -25,9 +24,6 @@ function App() {
       <Youtube title="Pick Up Song" videoUrl="https://www.youtube.com/watch?v=LAGf3Ikh56A" description="2023ボーカロイドコレクションネタ曲部門参加作品" />
       <Youtube title="My Work" videoUrl="https://www.youtube.com/watch?v=rc6u-vR2GAk" description="「きのこめあ」オリジナルPVの楽曲制作および、ゲーム内BGMを一部担当" />
       <HistoryTable items={historyItems} />
-      <button 
-        className="mt-6 bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
-        onClick={signOut}>Sign out</button>
       </main>
     </>
   );
